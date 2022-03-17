97c64b7ede8409d9f28d3b923204de6ca21a36bb
Paul Moreland <paul@pamosystems.com>
2022-03-17 22:06:43 +0100

    Added data folder to git

---
+++ data/Global/index.js
@@ -0,0 +1,13 @@
+/*
+ * Local Data
+ */
+
+import globals from './globals.json';
+import navTree from './nav-tree.json';
+import company from './company.json';
+
+export {
+    globals,
+    navTree,
+    company
+};
